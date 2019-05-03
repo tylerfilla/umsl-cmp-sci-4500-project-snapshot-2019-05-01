7cd706ba3fad57caceae52984695426c99236aa1
Tyler Filla <tylerfilla@hotmail.com>
2019-05-03 01:46:07 -0500

    Add {fmt} dependency
    Right now, we still print to standard output, but we have formatting at least.

--- src/log.c
+++ src/log.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include "log.h"
 
+extern void log__temp_format_and_submit(struct log_form* form);
+
 void log__submit_form(struct log_form* form) {
-  printf("%s [%d]\n", form->msg_fmt, form->msg_fmt_args_num);
+  log__temp_format_and_submit(form); // TODO: Log to a MPSC ring buffer
 }
